0fa453b174c0c23b0f70c1e1d43b0bafae7cc056
SerjMan <Manuylov_serj@ukr.net>
2024-02-09 18:35:10 +0200

    HW 27. JS #12. Jasmine_1

--- src/main.js
+++ src/main.js
@@ -15,4 +15,13 @@ function ageClassification(age) {
     ? 'долгожители'
     : null;
 }
-console.log(ageClassification(100));
+
+function evenFn(n) {
+  let arr = [];
+
+  for (let i = 1; i <= n; i++) if (i % 2 === 0) arr.push(i);
+
+  return arr;
+}
+
+console.log(evenFn(50));
--- src/main.spec.js
+++ src/main.spec.js
@@ -28,3 +28,32 @@ describe('ageClassification', function () {
     expect(ageClassification(130)).toBe(null);
   });
 });
+
+describe('evenFn', function () {
+  it('вернуть правильный массив', function () {
+    expect(evenFn(10)).toEqual([2, 4, 6, 8, 10]);
+  });
+  it('вернуть правильный массив', function () {
+    expect(evenFn(15)).toEqual([2, 4, 6, 8, 10, 12, 14]);
+  });
+  it('вернуть правильный массив', function () {
+    expect(evenFn(20)).toEqual([2, 4, 6, 8, 10, 12, 14, 16, 18, 20]);
+  });
+  it('вернуть правильный массив', function () {
+    expect(evenFn(30)).toEqual([
+      2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30,
+    ]);
+  });
+  it('вернуть правильный массив', function () {
+    expect(evenFn(40)).toEqual([
+      2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38,
+      40,
+    ]);
+  });
+  it('вернуть правильный массив', function () {
+    expect(evenFn(50)).toEqual([
+      2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38,
+      40, 42, 44, 46, 48, 50,
+    ]);
+  });
+});
